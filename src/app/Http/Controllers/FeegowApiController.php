e9fe6e9db632353c29a54d03ae04cf73da0ca385
Paulo Teixeira <paulo.teixeira@alliedbrasil.com.br>
2022-09-15 10:46:30 -0300

    refinamento

--- src/app/Http/Controllers/FeegowApiController.php
+++ src/app/Http/Controllers/FeegowApiController.php
@@ -4,16 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Session;
-use App\Http\Helpers\ApiHelper;
 
 class FeegowApiController extends Controller {
-    private $api;
-
+    use \App\Http\Helpers\ApiHelper;
+    
     public function listSpecialties(Request $request) {
-        $this->api          = new ApiHelper(); 
-        $this->api->header  = ["x-access-token" => env("FEEGOW_API_TOKEN"), "Content-Type" => "application/json"];
-        $this->api->url     = env("FEEGOW_URL_LISTA_ESPECIALIDADES");
-        $response           = $this->api->get();
+        $this->header  = ["x-access-token" => env("FEEGOW_API_TOKEN"), "Content-Type" => "application/json"];
+        $this->url     = env("FEEGOW_URL_LISTA_ESPECIALIDADES");
+        $response           = $this->get();
         $contents           = json_decode($response->getBody()->getContents());
         $result             = false;
 
@@ -25,10 +23,9 @@ class FeegowApiController extends Controller {
 
     public function listProfessionals(Request $request) {
         $especialidade_id    = $request->post("especialidade_id");
-        $this->api          = new ApiHelper(); 
-        $this->api->header  = ["x-access-token" => env("FEEGOW_API_TOKEN"), "Content-Type" => "application/json"];
-        $this->api->url     = env("FEEGOW_URL_LISTA_PROFISSIONAIS") . "?especialidade_id={$especialidade_id}&ativo=1";
-        $response           = $this->api->get();
+        $this->header  = ["x-access-token" => env("FEEGOW_API_TOKEN"), "Content-Type" => "application/json"];
+        $this->url     = env("FEEGOW_URL_LISTA_PROFISSIONAIS") . "?especialidade_id={$especialidade_id}&ativo=1";
+        $response           = $this->get();
         $contents           = json_decode($response->getBody()->getContents());
         $result             = false;
 
@@ -39,10 +36,9 @@ class FeegowApiController extends Controller {
     }
 
     public function listPatiantSource(Request $request) {
-        $this->api          = new ApiHelper(); 
-        $this->api->header  = ["x-access-token" => env("FEEGOW_API_TOKEN"), "Content-Type" => "application/json"];
-        $this->api->url     = env("FEEGOW_URL_LISTA_PATIENT_SOURCE");
-        $response           = $this->api->get();
+        $this->header  = ["x-access-token" => env("FEEGOW_API_TOKEN"), "Content-Type" => "application/json"];
+        $this->url     = env("FEEGOW_URL_LISTA_PATIENT_SOURCE");
+        $response           = $this->get();
         $contents           = json_decode($response->getBody()->getContents());
         $result             = false;
 
--- src/app/Models/Appointments.php
+++ src/app/Models/Appointments.php
@@ -23,25 +23,6 @@ class Appointments extends Model
     ];
     protected $appends			= [];
 
-    public function doSave () : Appointments {
-        if( false == empty($this->date_time) )
-            $this->date_time = \DateTime::createFromFormat('d/m/Y', $this->date_time)->format("Y-m-d h:i:s");
-
-        $this->birthdate = \DateTime::createFromFormat('d/m/Y', $this->birthdate)->format("Y-m-d");
-		$this->save();
-
-		return $this;
-	}
-
-    public function doUpdate () : Appointment {
-        if( false == empty($this->date_time) )
-            $this->date_time = \DateTime::createFromFormat('d/m/Y', $this->date_time)->format("Y-m-d");
-
-		$this->save();
-
-		return $this;
-    }
-
     protected function getDateTimeFormatadoAttribute ()
 	{
 		return date('d/m/Y H:i:s', strtotime($this->attributes['date_time']));
@@ -50,6 +31,23 @@ class Appointments extends Model
     protected function getBirthDateFormatadoAttribute ()
 	{
 		return date('d/m/Y', strtotime($this->attributes['birthdate']));
+	}
+
+	public static function boot()
+    {
+		parent::boot();
+
+		static::creating(function ($model) {
+			if( false == empty($model->date_time) )
+				$model->date_time = \DateTime::createFromFormat('d/m/Y', $model->date_time)->format("Y-m-d h:i:s");
+	
+			$model->birthdate = \DateTime::createFromFormat('d/m/Y', $model->birthdate)->format("Y-m-d");
+		});
+
+		static::updating(function ($model) {
+			if( false == empty($model->date_time) )
+				$model->date_time = \DateTime::createFromFormat('d/m/Y', $model->date_time)->format("Y-m-d h:i:s");
+		});
 	}
 
 }
